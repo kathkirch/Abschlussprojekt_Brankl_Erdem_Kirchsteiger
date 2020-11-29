c5e43a8df8ab8901c339e00d2ca756aa49194eab
Katharina <Katharina@192.168.1.213>
2020-11-29 17:09:38 +0100

    insert new Thread for DB, GUI1 and GUI2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 
 
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.SwingUtilities;
 
@@ -19,70 +20,40 @@ public class App {
     /**
      * @param args the command line arguments
      */
+   
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
-        
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PatqueueEingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PatqueueEingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PatqueueEingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PatqueueEingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+        //////////////////////////Programm 1 Patqueue Eingabe/////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new PatqueueEingabe().setVisible(true);
             }
         });
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////Programm 2a Datenbank ein- und auslesen///////////////////////////////////////////
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       
         
+        Datenbank.dbVerbinden();
         
+        Thread t = new RunDB();
+        t.start();
         
-        Datenbank myDB = new Datenbank();
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////Programm 2b GUI Masken//////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
         
-        myDB.dbVerbinden();
-        
-        List <Patienten> patienten = myDB.patqueueAuslesen();
-        List <Aufenthalte> aufenthaltListe = myDB.patientToAufenthalt(patienten);
-        //System.out.println("Patienten in Patqueue" + patienten);
-        
-        List <Aufenthalte> myDBaufenthalte = myDB.readAufenthalteDB();
-        //System.out.println("Meine Aufenthalte aus der DB" + myDBaufenthalte);
-        
-        List <Patlog> meinePatlogListe = myDB.patientToAufenthaltDB(aufenthaltListe);
-        myDB.insertPatlogDB(meinePatlogListe);
-        
-        //List <Patlog> myPatlogDB = myDB.readPatlog();
-        //System.out.println("Meine Patlogs aus der DB: " + myPatlogDB);        
-        
-        
-        new GUI2();
-        new GUI1();
-        
-        
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /*SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new GUI2();
-                new GUI1();
-            }
-        });*/
+        Datenbank.dbVerbinden();
+        Thread g1 = new GUI1();
+        Thread g2 = new GUI2();
+        g1.start();
+        g2.start();
 
         //myDB.dbSchliessen();
     } 
